126a64dd411738e304ee168eed9bbd523bba8b98
vanoeski <evanjanoski@live.com>
2026-03-03 16:07:59 -0500

    adding private dns and creating storage account

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,10 @@
+resource "random_string" "storage_suffix" {
+  length          = 6
+  upper           = false
+  min_numeric     = 1
+  special         = false
+}
+
 resource "azurerm_resource_group" "avd" {
   name     = "rg-avd-${var.environment}"
   location = var.location
@@ -23,8 +30,8 @@ module "networking" {
         name = "avd-poc-avd-hosts-snet"}
     }
 
-    Environment = "poc"
-    Project = "avd-capstone"
+    environment = "poc"
+    project = "avd-capstone"
     vnet_name = "avd-poc-vnet"
     resource_group_name = azurerm_resource_group.avd.name
     vnet_address_space = ["10.0.0.0/16"]
@@ -32,4 +39,24 @@ module "networking" {
     storage_subnet_key = "storage"
     nat_gateway_name = "avd-poc-nat-gateway"
 
+}
+
+module "storage" {
+    source = "../../modules/storage"
+
+    environment = "poc"
+    project = "avd-capstone"
+    storage_account_name = "stavdpoc${random_string.storage_suffix.result}"
+    resource_group_name = azurerm_resource_group.avd.name
+    location = var.location
+    access_tier = "Hot"
+    account_kind = "FileStorage"
+    account_tier = "Premium"
+    account_replication_type = "LRS"
+    share_name = "avdpocshare"
+    storage_share_quota = 5120
+    private_endpoint_name = "avdpocstoragepe"
+    private_service_connection_name = "avdpocstoragepsc"
+    private_dns_zone_id = module.networking.private_dns_zone_id
+    subnet_id = module.networking.storage_subnet_id
 }
