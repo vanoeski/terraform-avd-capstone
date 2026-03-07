e6e7e37ad1af179688b857fb08371a386bef8b1d
vanoeski <evanjj@protonmail.com>
2026-03-06 21:11:29 -0500

    more work on getting keyvault to work

--- modules/avd-hostpool/main.tf
+++ modules/avd-hostpool/main.tf
@@ -70,7 +70,7 @@ resource "azurerm_windows_virtual_machine" "avd_vm" {
   size                = var.vm_size
 
   admin_username = var.admin_username
-  admin_password = data.azurerm_key_vault_secret.avd_admin_password.value
+  admin_password = var.admin_password
 
   network_interface_ids = [azurerm_network_interface.avd_nic[each.key].id]
 
